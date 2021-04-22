860a1930e1ae0ce95bb6845e48d363780abb1749
kk <kk@kk.com>
2021-04-22 15:19:20 +0800

    V1.0

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "./vendor/autoload.php";
+
+
+//验证
+$msg = "must sign:123";
+$address = "0x95F64eCd5426e439c177846ac8Aadb1AC7aAE027";
+$sign = '0x6171238faa84cb9b938900acce9ff487a8757da9a906ef89c800c2d38c6240d320b1abe3b087afac8be6ed09db2c8bf19ae9388fbcda8b35f93e766a8abd5f691b';
+
+$signVerify = new \zkoka\SignVerify();
+$result = $signVerify ->verify($msg, $sign, $address);
+var_dump($result);
