58cb8fcd53ecd623878d33f5767b2335a36217ae
Alexey Sukhikh <lexerom@gmail.com>
2021-12-16 11:15:08 +0300

    Fix sign verify for php > 7.0

--- src/SignVerify.php
+++ src/SignVerify.php
@@ -35,7 +35,7 @@ class SignVerify
 
         $r = substr($sign, 2, 64);
         $s = substr($sign, 66, 64);
-        $v = (int)substr($sign, -2, 2);
+        $v = ord(hex2bin(substr($sign, 130, 2))) - 27;
 
         if ($v != ($v & 1)) {
             throw new \InvalidArgumentException('Invalid signature');
